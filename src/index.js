b9042a13b42c3160417f9c87c27f625b826c5cda
Alena Chyrskaya <helen.dubinskaya@gmail.com>
2019-03-06 00:06:56 +0100

    feat: new function for checking balanced brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+
+  let chars = str.split(''),
+      stack = [],
+      len = chars.length;
+
+      if (len%2==0) {
+        for (let i = 0; i < len; i++) {
+          for (let j = 0; j < bracketsConfig.length; j++) {
+            if (chars[i]==bracketsConfig[j][0] && bracketsConfig[j][0] !== bracketsConfig[j][1])  {
+              stack.push(bracketsConfig[j][1]);
+              continue;
+            } else if (chars[i]==bracketsConfig[j][1]) {
+                if (chars[i] == stack[stack.length-1]) {
+                 stack.pop();
+                } else {
+                    stack.push(bracketsConfig[j][1]);
+                  }
+              }
+          }
+        }
+        if (stack.length !== 0) {
+          return false;
+        } else  return true;
+      } else return false;
+  }
